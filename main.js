558fe3509c96c2f8a8843714d05b5c6bf7896a0a
L3OL1N <z3650888@gmail.com>
2022-10-09 16:45:09 +0800

    Feature server

--- main.js
+++ main.js
@@ -1,14 +1,51 @@
 const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
+const fs = require("fs");
+const hostname = 'localhost';
+const port = 8080;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+    if(req.url === "/"){
+        fs.readFile("index.html",function(error,data){
+            if(error) throw error;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            res.end();
+        })
+    }
+    else if(req.url === "/about"){
+        fs.readFile("about.html",function(error,data){
+            if(error) throw error;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            res.end();
+        })
+    }
+    else if(req.url === "/contact-me"){
+        fs.readFile("contact-me.html",function(error,data){
+            if(error) throw error;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            res.end();
+        })
+    }
+    else{
+        fs.readFile("404.html",function(error,data){
+            if(error) throw error;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            res.end();
+        })
+    }
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+server.listen(port,function(error){
+    if(error){
+        console.log("Something wrong",error);
+    }
+    else{
+        console.log("Server is listening on port");
+    }
+})
